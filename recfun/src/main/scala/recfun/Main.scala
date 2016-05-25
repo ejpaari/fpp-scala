eda996fcdb813f3f9fb43b9aec37b8eb546d925a
JP <juhapekka.arimaa@gmail.com>
2016-05-25 20:31:09 +0300

    Implemented balance and countChange

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -23,10 +23,33 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+  def balance(chars: List[Char]): Boolean = {
+    def isBalance(openBraces: Int, chars: List[Char]): Boolean = {
+      if (chars.isEmpty)
+        openBraces == 0
+      else if (openBraces < 0)
+        false
+      else {
+        if (chars.head == '(')
+          isBalance(openBraces + 1, chars.tail)
+        else if (chars.head == ')')
+          isBalance(openBraces - 1, chars.tail)
+        else
+          isBalance(openBraces, chars.tail)
+      }
+    }
+    isBalance(0, chars)
+  }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0)
+      1
+    else if (money < 0 || coins.isEmpty)
+      0
+    else
+      countChange(money, coins.tail) + countChange(money-coins.head, coins)
   }
+}
