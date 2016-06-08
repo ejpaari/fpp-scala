f53d24399c84b785cef402ce306d9e52b8c99ae2
JP <juhapekka.arimaa@gmail.com>
2016-06-08 20:31:49 +0300

    Implemented trending tweet sets

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -189,14 +189,20 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  def filterTweetByList(l: List[String], set: TweetSet) = {
+    set.filter(tweet => l.exists(word => tweet.text.contains(word)))
+  }
 
-  /**
-    * A list of all tweets mentioning a keyword from either apple or google,
-    * sorted by the number of retweets.
-    */
-  lazy val trending: TweetList = ???
+  lazy val allTweets: TweetSet = TweetReader.allTweets
+
+  lazy val googleTweets: TweetSet =
+    filterTweetByList(google, allTweets)
+
+  lazy val appleTweets: TweetSet =
+    filterTweetByList(apple, allTweets)
+
+  lazy val trending: TweetList =
+    googleTweets.union(appleTweets).descendingByRetweet
 }
 
 object Main extends App {
